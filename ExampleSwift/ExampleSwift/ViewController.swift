7bab25a4925e9092989923782b614b6951c1d65b
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-07-29 10:32:32 -0300

    adding button to view

--- ExampleSwift/ExampleSwift/ViewController.swift
+++ ExampleSwift/ExampleSwift/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import ModalCustomizationFramework
 
 class ViewController: UIViewController {
-
+    
+    lazy var triggerButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.setTitle("Trigger button", for: .normal)
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(buttonAction), for:.touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(triggerButton)
+        setButtonConstraints()
         // Do any additional setup after loading the view.
     }
+    
+    @objc func buttonAction() {
 
+    }
+    
+    func setButtonConstraints() {
+        NSLayoutConstraint.activate([
+            triggerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            triggerButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            triggerButton.widthAnchor.constraint(equalToConstant: 200) ,
+            triggerButton.heightAnchor.constraint(equalToConstant:40)])
+    }
 
 }
 
